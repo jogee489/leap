147e8c506be64a3ced0deb5ce347e23865a36244
Aaron <aschlorke@yahoo.com>
2016-02-04 13:40:22 -0500

    Worked on FoodItem views and controller

--- leap/public/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
-/**
- * Check to see if a given string is blank.
- */
-function isBlank(str) {
-  return (!str || /^\s*$/.test(str));
-}
