de4573dfce48e8ae95f5ad70052ada2c3efdd371
Aaron <aschlorke@yahoo.com>
2016-02-08 20:11:23 -0500

    Fixed

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,58 @@
+class RecipesController < ApplicationController
+
+	def save_recipe_json
+		
+	end
+	
+	def searchRecipes
+	end
+
+	def index
+		@recipeList = Recipe.all
+	end
+
+	def list
+		@recipeList = Recipe.all.page(params[:page]).per_page(3)
+	end
+
+	def show
+		@recipe = Recipe.find(params[:id])
+		if @recipe.blank?
+			flash[:error] = 'The recipe you are looking for has dissapeared..'
+			render action: 'list'
+		end
+	end
+
+	def new
+		@recipe = Recipe.new()
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+		if (@recipe.save) 
+			flash[:notice] = 'The new recipe was successfully added.'
+			redirect_to :action => 'show', :id => @recipe.id
+		else 
+			render action: 'new'
+		end
+	end
+
+	def edit
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def update
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def destroy
+		@recipe = Recipe.find(params[:id]).destroy
+    	redirect_to(:action => 'index')
+	end
+
+  private
+    def recipe_params                                                                                                                         
+      params.require(:recipe).permit(:title, :ingredients, :directions)
+    end
+	
+end
