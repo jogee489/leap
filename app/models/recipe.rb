3a889b7295c8c538b93d3e4fa79ce32a346b5051
Aaron <aschlorke@yahoo.com>
2016-03-09 16:39:59 -0500

    added sort methods to some models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,7 @@ class Recipe < ActiveRecord::Base
 	scope :sorted, lambda { order("recipes.title ASC")}
 	  validates_uniqueness_of :title
 
-
 	def title=(s)
-    write_attribute(:title, s.to_s.titleize) # The to_s is in case you get nil/non-string
-  end
+    	write_attribute(:title, s.to_s.titleize) # The to_s is in case you get nil/non-string
+  	end
 end
