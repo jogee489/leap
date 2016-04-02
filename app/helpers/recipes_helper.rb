817821024b5b540c1420ceecdf067fbad41f1d7b
JJ Dorko <jdorko90@gmail.com>
2016-04-02 10:41:45 -0400

    Added check for if recipes are present for the recipe table

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -4,13 +4,13 @@ module RecipesHelper
 	# Render table for listing recipes
 	def recipe_list(recipes)
 		recipe_table = ''
-		
-		recipes.each_with_index do |recipe, index|
-			
+		if recipes.present?
+			recipes.each_with_index do |recipe, index|
+				
 
-			id_element = add_form_element('recipe', 'id', {type: 'hidden'}, {value: recipe.try(:id)})
-			form = render('recipes/form', recipe: recipe)
-			recipe_table << <<-EOS.html_safe
+				id_element = add_form_element('recipe', 'id', {type: 'hidden'}, {value: recipe.try(:id)})
+				form = render('recipes/form', recipe: recipe)
+				recipe_table << <<-EOS.html_safe
 
 <tr class="app-table-tr">
 	#{id_element}
@@ -29,6 +29,15 @@ module RecipesHelper
     </td>
 </tr>
 
+				EOS
+			end
+		else
+			recipe_table << <<-EOS.html_safe
+<tr class="app-table-tr">
+	<td class="app-table-title" style="text-align: center;">
+		No recipes were found.
+	</td>
+</tr>
 			EOS
 		end
 		recipe_table.html_safe
